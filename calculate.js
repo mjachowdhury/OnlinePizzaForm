454664517adb3bbc9dc2fca64d633bd3aa318a00
mjachowdhury <study.computing2016@gmail.com>
2016-11-25 19:16:52 +0000

    Added files and foulder for online pizza from
    It has css and javascript anf html files and pictures

---
+++ calculate.js
@@ -0,0 +1,195 @@
+ //Author: Mohammed Alom Student ID: R00144214 Email: mohammed.alom@mycit.ie
+
+
+//This function will checks which options are checked and adds the amount to sum to get the total amount
+//if deselect from the check box then price will automatically will update in the form
+
+var image1 = new Image()
+image1.src = "images/header1.jpg";
+
+var image2 = new Image()
+image2.src = "images/header2.jpg";
+
+var image3 = new Image()
+image3.src = "images/header3.png";
+
+var image4 = new Image()
+image4.src = "images/header4.jpg";
+
+var step = 1;
+
+function slideit()
+{
+	document.images.slide.src=eval("image"+step+".src");
+	if(step<4){
+	step++;}
+	else
+	{step=1;}
+	setTimeout("slideit()",2500);
+}
+
+/*
+var myImage = document.getElementById("myPhoto");
+
+var imageArray=["images/header1.jpeg","images/header2.jpeg","images/header3.jpeg","images/header4.jpeg","images/header5.jpeg"];
+
+var imageIndex=0;
+function changeImage()
+{
+	myPhoto.setAttribute("src",imageArray [imageIndex]);
+	imageIndex++;
+	if(imageIndex>=imageArray.length)
+	{
+		imageIndex=0;
+	}
+	
+}
+
+var intervarHandle = setInterval(changeImage,2000);
+
+myPhoto.onclick=function()
+{
+	clearInterval(intervarHandle);
+}
+
+*/
+
+function sumTotal() 
+{
+  var sum = 0; // var sum is variable & will store all the prie value 
+ 
+  if (document.getElementById('Small').checked ) 
+  {
+    sum = sum + 9.99;
+	
+  }
+  if (document.getElementById('Medium').checked) 
+  {
+    sum = sum + 12.99;
+  }
+  if (document.getElementById('Large').checked) 
+  {
+    sum = sum + 14.99;
+  }
+  if (document.getElementById('ExtraLarge').checked) 
+  {
+    sum = sum + 15.99;
+  }
+  if (document.getElementById('Onion').checked)
+  {
+    sum = sum + 0.50;
+  }
+  if (document.getElementById('Olive').checked) 
+  {
+    sum = sum + 0.50;
+  }
+  if (document.getElementById('Chicken').checked) 
+  {
+    sum = sum + 1.00;
+  }
+  if (document.getElementById('Papperoni').checked) 
+  {
+    sum = sum + 1.00;
+  }
+  if (document.getElementById('Pappers').checked) 
+  {
+    sum = sum + 0.50;
+  }
+  if (document.getElementById('Email').checked) 
+  {
+	var discount=0.05;
+    sum = sum -(sum*discount);
+  }
+   
+  var total = document.getElementById("totalPrice"); //var total is variable & will work with id 'totalPrice'
+
+  total.style.display = 'block'; // will display total price as a block
+  total.innerHTML = "Total Price For The Mug  &euro;  " + sum.toFixed(2); // € is for euro sign and toFixed(2) is for total price will show after decemil value untill 2 number
+  var price = document.getElementById('tprice').value = sum.toFixed(2); //this part for the after submit to see in the form total price.
+}
+
+function submitForm() // This function for submit the form and also will give alert if nothing select
+{	
+	if (document.getElementById('tprice').value == '')
+	{
+		alert('Please Select Product Before Order');
+		return false;
+	}
+	return true;
+}
+
+ 
+function hideTotal() //This function for during onload the page total price for the mug will not show before select anything 
+{
+  var total = document.getElementById('totalPrice');
+  total.style.display = 'none';
+}
+
+
+/*
+//This function will checks the colour choosen and changes the image depending on the option
+function colourChange() 
+{
+  var size = document.getElementById("size").value; //var color is a variable and will store the color choose option
+  document.getElementById("pizzasize").src = "images/" + size + ".jpeg"; // All the image is .jpg format
+}
+*/
+
+  function pizzaSizeChange(id) {
+   var el = document.getElementById(id);
+
+   if (el.style.visibility=="visible") {
+          el.style.visibility="hidden";
+     }
+     else {
+          el.style.visibility="visible";
+     }
+ }
+
+ 
+ /*this is another mouse over event function
+ $(document).ready(function() {
+    $( "#myImage" ).mouseover(function(){
+        $(this).attr("src", "images/footer2.jpg");
+    });
+
+    $( "#myImage" ).mouseout(function(){
+        $(this).attr("src", "images/footer1.jpg");
+    });
+});
+ 
+ */
+ /* this function for mouseover event
+ 
+ $('#myImage').hover(function() {
+  $(this).attr('src', 'images/footer1.jpg');
+}, function() {
+  $(this).attr('src', 'images/footer2.jpg');
+});
+ 
+*/ 
+/* this function for mouse over evernt
+ function mouseOver()
+ {
+	 document.getElementById("myImage").src="images/footer2.jpg";
+ }
+
+ 
+ function mouseOut()
+ {
+	 document.getElementById("myImage").src="images/footer2.jpg";
+ }
+ 
+ 
+document.getElementById("myImage").addEventListener("mouseover", mouseOver);
+document.getElementById("myImage").addEventListener("mouseout", mouseOut);
+*/
+/*
+function mouseOver() {
+    document.getElementById("demo").style.color = "red";
+}
+
+function mouseOut() {
+    document.getElementById("demo").style.color = "black";
+}
+*/
